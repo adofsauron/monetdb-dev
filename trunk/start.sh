dbe5bb109a2859f296189ba90902f7d6e473d4b8
adofsauron <adofsauron@163.com>
2023-04-18 14:59:31 +0800

    fix

--- trunk/start.sh
+++ trunk/start.sh
@@ -12,13 +12,13 @@ touch /dbfarm/logs/trace.log
 # https://www.monetdb.org/documentation-Jan2022/admin-guide/manpages/mserver5/
 
 nohup /usr/local/bin/mserver5 \
-    --dbpath=/dbfarm/SF-0_001 \
+    --dbpath=/dbfarm/SF-0_1 \
     --dbtrace=/dbfarm/logs/trace.log \
     --config=/root/.monetdb \
-    --set merovingian_uri=mapi:monetdb://localhost.localdomain:50000/SF-0_001 \
+    --set merovingian_uri=mapi:monetdb://localhost.localdomain:50000/SF-0_1 \
     --set mapi_listenaddr=localhost \
-    --set mapi_usock=/dbfarm/SF-0_001/.mapi.sock \
-    --set monet_vault_key=/dbfarm/SF-0_001/.vaultkey \
+    --set mapi_usock=/dbfarm/SF-0_1/.mapi.sock \
+    --set monet_vault_key=/dbfarm/SF-0_1/.vaultkey \
     --set gdk_nr_threads=32 \
     --set max_clients=64 \
     --set sql_optimizer=default_pipe \
