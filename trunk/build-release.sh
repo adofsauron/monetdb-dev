0edbab15a401b49841dd6365169fa8080ef3d590
adofsauron <adofsauron@163.com>
2023-07-03 17:27:52 +0800

    fix

--- trunk/build-release.sh
+++ trunk/build-release.sh
@@ -4,7 +4,7 @@ cd monetdb-11.41.31
 
 mkdir -p build
 cd build
-cmake  -DCMAKE_BUILD_TYPE=Release --enable-gdk --enable-monetdb5 --enable-rdf --enable-datacell --enable-fits --enable-sql --enable-geom --enable-jaql --enable-gsl --enable-odbc --enable-testing --enable-console --enable-jdbc ..
+cmake  -DCMAKE_BUILD_TYPE=Release ..
 make -j`nproc`
 make install
 
