aff0bca5ac3ab6863f55d7b0f3eb4faadb7b0634
adofsauron <adofsauron@163.com>
2023-09-22 16:10:00 +0800

    fix

--- trunk/yum.sh
+++ trunk/yum.sh
@@ -1,3 +1,31 @@
 #!/bin/bash
 
 yum install -y bison
+
+yum install -y atop		
+yum install -y bmon		
+yum install -y cmatrix	
+yum install -y dnstop	
+yum install -y ethstatus
+yum install -y glances
+yum install -y htop
+yum install -y ifstat
+yum install -y iotop
+yum install -y iptotal
+yum install -y iptraf-ng
+yum install -y itop
+yum install -y jnettop
+yum install -y kerneltop
+yum install -y latencytop
+yum install -y logtop
+yum install -y netmrg
+yum install -y nload
+yum install -y nmon
+yum install -y ntop
+yum install -y powertop
+yum install -y sagan
+yum install -y slurm
+yum install -y snetz
+yum install -y top
+yum install -y tiptop
+yum install -y vnstat
